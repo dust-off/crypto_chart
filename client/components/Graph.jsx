e1fa0405f4785ff799d7f2c882fb912bbe4fd874
Dustin <dustinburns@gmail.com>
2018-04-01 09:30:24 -0700

    refactor the api information for graph readability

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -27,6 +27,21 @@ const chartData = {
     ]
 }
 
+const dataToChart = (raw) => {
+    data =  {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+            datasets: [{
+                label: 'apples',
+                data: [12, 19, 3, 17, 6, 3, 7],
+                backgroundColor: "rgba(153,255,51,0.4)"
+            }, {
+                label: 'oranges',
+                data: [2, 29, 5, 5, 2, 3, 10],
+                backgroundColor: "rgba(255,153,0,0.4)"
+            }]
+    }
+}
+
 const Graph = ({ test }) => {
     // console.log(props);
     return (
